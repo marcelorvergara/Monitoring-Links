3cf8eafd26cda2d3ff04cdd7e53ac7087ddf76a7
Marcelo Vergara <marcelorv@gmail.com>
2022-11-19 19:15:25 -0300

    SEO improvments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Monitoring Links - Best Way to Monitor your Website Link</title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
