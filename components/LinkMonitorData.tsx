c78f219528f7199c1ac2d1ba982b1d74f13be91a
Marcelo Vergara <marcelorv@gmail.com>
2022-12-04 08:18:29 -0300

    protocol selection in the registring process

--- components/LinkMonitorData.tsx
+++ components/LinkMonitorData.tsx
@@ -1,3 +1,4 @@
+import { E } from "chart.js/dist/chunks/helpers.core";
 import { SetStateAction, useEffect, useState } from "react";
 import { getUrlStatus, setUrlMonitor } from "../helpers/helpers";
 import { IFeedback } from "../interfaces/IFeedback";
@@ -12,6 +13,7 @@ interface ILinkMonitorDataProps {
 
 export default function LinkMonitorData(props: ILinkMonitorDataProps) {
   const [url, setUrl] = useState<string>("");
+  const [protocol, setProtocol] = useState<string>("https://");
   const [feedback, setFeedback] = useState<IFeedback>({});
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
@@ -31,9 +33,15 @@ export default function LinkMonitorData(props: ILinkMonitorDataProps) {
 
   async function registerUrl(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault(); // fetch get cancelled when this is not present
+    if (url.includes("http://") || url.includes("https://")) {
+      setFeedback({
+        error: "Invalid URL. Do not use http or https in the url string",
+      });
+      return;
+    }
     setIsLoading(true);
     try {
-      const resp = await setUrlMonitor(url, props.userInfo.id);
+      const resp = await setUrlMonitor(protocol + url, props.userInfo.id);
       const respJson = await resp.json();
       if (resp.status === 201) {
         setFeedback(respJson);
@@ -48,6 +56,10 @@ export default function LinkMonitorData(props: ILinkMonitorDataProps) {
     }
   }
 
+  function onProtocolChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setProtocol(event.currentTarget.value);
+  }
+
   return (
     <form className="flex flex-wrap justify-center mt-2">
       <div className="w-full sm:w-6/12 text-left ">
@@ -60,13 +72,31 @@ export default function LinkMonitorData(props: ILinkMonitorDataProps) {
             htmlFor="grid-last-name">
             Enter a URL to monitor:
           </label>
+          <div className="flex gap-4 items-center justify-start text-sm mt-4 md:w-full m-4">
+            <input
+              onChange={(e) => onProtocolChange(e)}
+              type="radio"
+              value="https://"
+              name="https://"
+              checked={protocol === "https://"}
+            />
+            HTTPS
+            <input
+              onChange={(e) => onProtocolChange(e)}
+              type="radio"
+              value="http://"
+              name="http://"
+              checked={protocol === "http://"}
+            />
+            HTTP
+          </div>
           <input
             onChange={handleUrlValue}
             value={url}
             className="appearance-none block text-xs w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="text"
-            placeholder="http://www.google.com"
+            placeholder="www.google.com"
           />
         </div>
         <div className="text-sm mt-4 md:w-full md:px-8 m-4 flex justify-end">
