d0c3e34edb2ee0be6c1dc6330840275e9c834898
Marcelo Vergara <marcelorv@gmail.com>
2022-12-05 15:30:17 -0300

    fix exit button position in confirm dialog

--- components/confirmDialog/BasicDialog.tsx
+++ components/confirmDialog/BasicDialog.tsx
@@ -15,9 +15,9 @@ export default function BasicDialog(props: IBasicDialogProps) {
   return (
     <div className="flex justify-center items-center">
       <div className="fixed top-24 z-50 overflow-auto bg-smoke-light flex m-4">
-        <div className="relative px-8 py-4 bg-gray-100 w-full max-w-md m-auto flex-col flex rounded-lg">
+        <div className="relative px-8 py-6 bg-gray-100 w-full max-w-md m-auto flex-col flex rounded-lg">
           <div>{props.children}</div>
-          <span className="absolute top-0 right-0 p-2">
+          <span className="absolute -top-1 right-0">
             <IconButton onClick={() => onClose()}>
               <ExitIcon />
             </IconButton>
